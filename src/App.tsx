a6b48bdf275196885dad83d8cc964975a82abd37
renatorrocha <renatorrodrigues2002@gmail.com>
2024-10-09 22:53:56 -0300

    feat: input card ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { CornerDownLeft } from "lucide-react";
 import Logo from "/logo.svg";
+import InputId from "./components/button";
 
 export default function App() {
 	return (
@@ -11,19 +11,7 @@ export default function App() {
 					className="w-[100px] h-12 mx-auto mb-4"
 				/>
 
-				<div className="py-2 pr-4 pl-2 border rounded-xl border-base-gray-400 flex gap-3">
-					<input
-						placeholder="ID do Cartão"
-						className="placeholder:text-base-gray-500 placeholder:text-md border-0 flex-1"
-					/>
-
-					<button
-						type="submit"
-						className="h-10 w-10 bg-base-gray-700/60 items-center rounded-xl"
-					>
-						<CornerDownLeft className="size-5 text-base-white m-auto" />
-					</button>
-				</div>
+				<InputId />
 			</div>
 		</main>
 	);
---
+++ src/components/button.tsx
@@ -0,0 +1,29 @@
+import { CornerDownLeft } from "lucide-react";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function InputId() {
+	const [cardId, setCardId] = useState("");
+
+	return (
+		<div className="py-2 pr-4 pl-2 border rounded-xl border-base-gray-400 bg-base-gray-300 flex gap-3 focus-within:border-principal-pink transition-all duration-300">
+			<input
+				type="text"
+				placeholder="ID do Cartão"
+				className="placeholder:text-base-gray-500 placeholder:text-md text-base-gray-700 caret-principal-pink bg-transparent border-0 flex-1 focus:outline-none focus:border-transparent focus:ring-0 text-md"
+				value={cardId}
+				onChange={(e) => setCardId(e.target.value)}
+			/>
+
+			<button
+				type="submit"
+				className={twMerge(
+					"h-10 w-10 bg-base-gray-700/60 items-center rounded-xl transition-all duration-300",
+					cardId && "bg-base-gray-700",
+				)}
+			>
+				<CornerDownLeft className="size-5 text-base-white m-auto" />
+			</button>
+		</div>
+	);
+}
