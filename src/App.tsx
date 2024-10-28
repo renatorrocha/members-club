f3fe1a66b062b29f5617edb838bf5c18dfdf7fe9
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-10-28 11:15:40 -0300

    wip: loyalty card

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import Logo from "/logo.svg";
 import InputCardId from "./components/input-card-id";
 import UserCard from "./components/user-card";
-import FidelityCard from "./components/fidelity-card";
+import LoyaltyCard from './components/loyalty-card';
+
+const data = {
+    id: "207-245-699-104",
+    name: "Capitão Nascimento",
+    clientSince: "12/04/2023",
+    appointmentHistory: [
+        {
+            date: "01/05/2024",
+            time: "10:00",
+        },
+        {
+            date: "12/02/2024",
+            time: "09:00",
+        },
+        {
+            date: "30/12/2023",
+            time: "11:00",
+        },
+        {
+            date: "05/11/2023",
+            time: "10:30",
+        },
+    ],
+    loyaltyCard: {
+        totalCuts: 4,
+        cutsNeeded: 10,
+        cutsRemaining: 6,
+    },
+};
 
 export default function App() {
-	return (
-		<main className="px-4 pt-8 mx-auto bg-base-gray-200 h-screen">
-			<div className="border w-full items-center">
-				<img
-					src={Logo}
-					alt="Member Club logo"
-					className="w-[100px] h-12 mx-auto mb-4"
-				/>
+    return (
+        <main className="px-4 pt-8 mx-auto bg-base-gray-200 h-screen">
+            <div className="border w-full items-center">
+                <img
+                    src={Logo}
+                    alt="Member Club logo"
+                    className="w-[100px] h-12 mx-auto mb-4"
+                />
 
-				<InputCardId />
+                <InputCardId />
 
-				<UserCard />
+                <UserCard />
 
-				<FidelityCard />
-			</div>
-		</main>
-	);
+                <LoyaltyCard />
+            </div>
+        </main>
+    );
 }
---
+++ src/components/loyalty-card.tsx
@@ -0,0 +1,56 @@
+import { Gift } from "lucide-react";
+import Card from "./card";
+import CheckPin from "/check-pin.png";
+
+const data = {
+    id: "207-245-699-104",
+    loyaltyCard: {
+        totalCuts: 4,
+        cutsNeeded: 10,
+        cutsRemaining: 6,
+    },
+};
+
+export default function LoyaltyCard() {
+    return (
+        <Card className="flex-col">
+            <div className="flex items-start justify-between w-full">
+                <div className="space-y-1">
+                    <p className="text-base-gray-500 subtitle-sm">
+                        Cartão Fidelidade
+                    </p>
+                    <p className="text-xs text-base-gray-500">
+                        Ao fazer cortes de cabelo, o <br /> décimo sai de graça!
+                    </p>
+                </div>
+
+                <span className="px-2 bg-gray-200 py-1 rounded-3xl subtitle-sm text-base-gray-500">
+                    ID: {data.id}
+                </span>
+            </div>
+
+            <section className="flex flex-wrap gap-2 items-center justify-center">
+                {Array.from({ length: data.loyaltyCard.cutsNeeded }).map(
+                    (_, index) => (
+                        <div
+                            key={index}
+                            className="size-14 rounded-lg bg-base-gray-200 flex items-center justify-center"
+                        >
+                            {index < data.loyaltyCard.totalCuts && (
+                                <img
+                                    src={CheckPin}
+                                    alt=""
+                                    className="mx-auto size-10"
+                                />
+                            )}
+
+                            {index === data.loyaltyCard.cutsNeeded - 1 && (
+                                <Gift className="text-base-gray-500" />
+                            )}
+                        </div>
+                    )
+                )}
+            </section>
+        </Card>
+    );
+}
